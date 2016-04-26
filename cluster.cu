727aa602d58a8e13d24792468ce1803df427ee70
Bret Marshall <bdmarsh13@as0.cluster.earlham.edu>
2016-04-26 13:02:52 -0400

    new stuff

--- cluster.cu
+++ cluster.cu
@@ -1,44 +1,54 @@
-<<<<<<< HEAD
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/time.h>
 #include <time.h>
 #include <cmath>
 #include <cstdlib>
 #include <cstring>
+#include <cuda_runtime.h>
 
-
+__device__ int position;
 int lenString=594;
 int maxNumStrings = 1000000;                           
 int threshold = 2;
 
-__global__ void Compare(int position, char *d_a, int *d_b, int *d_c, int max_count, int lenString, int threshold){
+__global__ void kernel() { position = 3;}
+
+__global__ void search(int *d_b, int *d_c, int max_count)
+{
 	int my_id = blockDim.x * blockIdx.x + threadIdx.x;
+	if(d_c[my_id]==0 && (d_b[my_id] > d_b[position])&&(my_id < max_count))
+	{
+		position = my_id;
+	}
+}
+
+__global__ void Compare(char *d_a, int *d_b, int *d_c, int max_count, int lenString, int threshold){
+	int my_id = blockDim.x * blockIdx.x + threadIdx.x;
+	
+		
 	if ((my_id < max_count) && (d_c[my_id] == 0) && (my_id != position)){
-		int offset = my_id*lenString - position*lenString;
+		
 		int x, i, diffs = 0, stop =0;
-	for (x=0;x<lenString;x+=6){
-		for (i=0;i<6;i++){
-			diffs += (bool)(d_a[x+i+position]^d_a[x+i+offset+position]);
+		for (x=0;x<lenString;x++){
+			diffs += (bool)(d_a[(lenString*position)+x]^d_a[(my_id*lenString)+x]);
+			
 			if (diffs > threshold){
-				stop += 1;
 				break;}
 		}
 		
-		if (stop == 1)
-			break;
-	}
+		if (diffs <= threshold){
+			d_b[position] += d_b[my_id];
+			d_c[position] = 2;
+			d_c[my_id] = 1;
+			
+		}
 	
-	if (diffs <= threshold){
-		d_b[position] += d_b[my_id];
-		d_c[position] = 2;
-		d_c[my_id] = 1;
+		else {
+			d_c[position] = 2;
+		}
 	}
-	
-	else 
-	{
-		d_c[position] = 2;
-	}
-}
+
 }
 
 
@@ -50,6 +60,7 @@ int main(int argc, char** argv) {//allocation of variables
 	int i=0, actual_count=0;
 	int size_string = maxNumStrings*sizeof(char)*(lenString+1);
 	int size_int = maxNumStrings*sizeof(int);
+	int size_int_2 = sizeof(int);
 	struct timeval start, end; 				//using time
 	double wallTime;
 	cudaError_t status = (cudaError_t)0;
@@ -68,11 +79,10 @@ int main(int argc, char** argv) {//allocation of variables
 		fprintf(stderr, "malloc() FAILED (Block)\n"); 
 		exit(0);}
 	merged = (int *)malloc(size_int);	
-	
-
+	cudaMemset(&position,0,sizeof(int));
 
 	while( fscanf(fp,"%s %d", copy, &numbers) != EOF && actual_count <100){
-		strncpy(&strings[i],copy, lenString);
+		strcpy(&strings[i],copy);
 		counts[actual_count]=numbers;
 		//printf("%s\n", copy);
 		//printf("%s\n", &a[i]);
@@ -99,9 +109,14 @@ int main(int argc, char** argv) {//allocation of variables
 	int threads_num = 512, blocks_num;
 	blocks_num = (int)ceil((float)actual_count/threads_num);
 	
-	int position = 0;
-	Compare<<<blocks_num, threads_num>>>(position, d_a, d_b, d_c, actual_count, lenString, threshold);
-		
+	
+	
+	kernel<<<1,1>>>();
+	//search<<<blocks_num, threads_num>>>(d_b, d_c, actual_count);
+	Compare<<<blocks_num, threads_num>>>(d_a, d_b, d_c, actual_count, lenString, threshold);
+	
+
+	
 	cudaMemcpy(counts, d_b, size_int, cudaMemcpyDeviceToHost);
 	cudaMemcpy(merged, d_c, size_int, cudaMemcpyDeviceToHost);
 	
