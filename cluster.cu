c4de4c91235a4e94bf79610dc5a5458496b13dc1
Vitalii Stadnyk <vstadn14@earlham.edu>
2016-04-25 23:37:11 -0400

    all the updates from niraj and vitalii

--- cluster.cu
+++ cluster.cu
@@ -10,20 +10,53 @@ int lenString=594;
 int maxNumStrings = 1000000;                           
 int threshold = 2;
 
+__global__ void populate (int *d_b, int *copy_db, int *d_c, int size) {
+	int n = 0;
+	int my_id = blockDim.x * blockIdx.x + threadIdx.x;
+
+	if (my_id < size) {
+		n = abs((bool)d_c[my_id] - 1);
+		copy_db[my_id] = d_b[my_id] * n;
+	}		
+}
+__global__ void cuda_select(int *db, int size, int *largest)
+{
+	int my_id = blockDim.x * blockIdx.x + threadIdx.x;
+	if(my_id < size)
+	{
+		if(db[2*my_id] > db[2*my_id + 1])
+			db[my_id] = db[2*my_id];
+		else
+			db[my_id] = db[2*my_id + 1];
+	}
+}
+__host__ void select(int *db, int size, int *largest)
+{
+	int height = (int)ceil(log2((double)size));
+	int i = 0;
+	
+	for(i = 0; i < height; i++)
+	{
+		size = (int)ceil((double) size/2);
+		int threads_num = 512, blocks_num;
+		blocks_num = (int)ceil((float)size/threads_num);
+		cuda_select<<<blocks_num, threads_num>>>(db, size, largest);
+	}
+	*largest = db[0];
+}
 __global__ void Compare(int position, char *d_a, int *d_b, int *d_c, int max_count, int lenString, int threshold){
 	int my_id = blockDim.x * blockIdx.x + threadIdx.x;
 	if ((my_id < max_count) && (d_c[my_id] == 0) && (my_id != position)){
 		int offset = my_id*lenString - position*lenString;
 		int x, i, diffs = 0, stop =0;
-	for (x=0;x<lenString;x+=6){
-		for (i=0;i<6;i++){
-			diffs += (bool)(d_a[x+i+position]^d_a[x+i+offset+position]);
-			if (diffs > threshold){
-				stop += 1;
-				break;}
-		}
-		
-		if (stop == 1)
+		for (x=0;x<lenString;x+=6){
+			for (i=0;i<6;i++){
+				diffs += (bool)(d_a[x+i+position]^d_a[x+i+offset+position]);
+				if (diffs > threshold){
+					stop += 1;
+					break;}
+				}
+			if (stop == 1)
 			break;
 	}
 	
@@ -44,6 +77,7 @@ __global__ void Compare(int position, char *d_a, int *d_b, int *d_c, int max_cou
 int main(int argc, char** argv) {//allocation of variables
 	char *strings, *d_a;
 	int *counts, *merged, *d_b, *d_c;	//host copy of a
+	int *largest, *copy_db;
 	char copy[lenString+1]; //string to copy in info
 	int numbers=0;
 	int i=0, actual_count=0;
@@ -84,6 +118,7 @@ int main(int argc, char** argv) {//allocation of variables
 	cudaMalloc(&d_a, size_string);
 	cudaMalloc(&d_b, size_int);
 	cudaMalloc(&d_c, size_int);
+	cudaMalloc(&copy_db, size_int);
 	
 	for(int i =0; i<actual_count;i++)
 	{
@@ -97,6 +132,9 @@ int main(int argc, char** argv) {//allocation of variables
 	
 	int threads_num = 512, blocks_num;
 	blocks_num = (int)ceil((float)actual_count/threads_num);
+	
+	populate<<<blocks_num, threads_num>>>(d_b, copy_db, d_c, actual_count);
+	select(copy_db, actual_count, largest);
 	
 	int position = 0;
 	Compare<<<blocks_num, threads_num>>>(position, d_a, d_b, d_c, actual_count, lenString, threshold);
